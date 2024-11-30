1ab9735dd119f3e997b847292b4e0a9b2794a513
Stephany Ho <shohk39@gmail.com>
2024-11-29 23:58:42 -0500

    Update Readme

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # solo-project
+
+An application for reviewing Michelin-starred restaurants, allowing users to view listed restaurants on a map, leave reviews, and manage their content securely.
+
+## Features
+
+1. View Restaurants: Use Google Maps to locate listed Michelin restaurants
+2. Leave Reviews: Create, read, update, and delete reviews for listed restaurants (CRUD functionality)
+3. Protected Modifications: Review modifications are secured with JWT authorization
+
+## Tech Stack
+
+- Language: JavaScript, TypeScript
+- Front-End: React, Tailwind CSS
+- Back-End: Node.js, Express.js
+- Database: MongoDB
+- APIs: Google Maps
+
+## Installation and Usage
+
+1. Clone the repo:
+
+```bash
+git clone https://github.com/seneyu/solo-project.git
+```
+
+2. Install dependencies:
+
+```bash
+npm install
+```
+
+3. Set up `.env` file for the following items:
+
+- GOOGLE_MAPS_API_KEY
+- MONGO_URI
+- JWT_SECRET
+- JWT_REFRESH_SECRET
+
+4. Set up and connect to your MongoDB database. Navigate to ./server/seeds.ts and run the file to create seed restaurants and reviews in your database.
+
+5. Start the server:
+
+```bash
+npm run start
+```
+
+6. Open your browser and navigate to `http://localhost:3000`.
--- server/seeds.ts
+++ server/seeds.ts
@@ -22,6 +22,8 @@ async function main() {
           'After more than 30 years of operation, The Kitchen has upgraded to a grander space, but its raucous party atmosphere, led by energetic emcee Chef Kelly McCown, is the same as ever. Diners are encouraged to get up and walk around, even to wander through the kitchen and rub elbows with the cooks. Really like that truffle dish? The chef, who frequently proclaims the menu’s caloric impact, nonetheless invites you to ask for seconds. The menu changes monthly and features far-reaching inspiration while highlighting the Central Valley’s treasure trove of ingredients. Expect dishes like sweet potato cannelloni with brown butter sabayon, or perfectly seared scallops paired with cauliflower, blood orange, and lime leaf—and remember that the central premise is to have fun.',
         website: 'https://thekitchenrestaurant.com/',
         number: '+1 916-568-7171',
+        latitude: '38.5645656729266',
+        longitude: '-121.5012682755339',
       },
       {
         name: 'Localis',
@@ -32,6 +34,8 @@ async function main() {
           'Together with his tight-knit team, Chef/owner Christopher Barnum-Dann brings unusual warmth to this intimate setting. His enthusiasm is instantly palpable as he happily explains his inspiration behind particular dishes and even solicits feedback. This is especially true for diners who sit at the spacious counter. The freewheeling tasting menu is thoroughly Californian in its commitment to carefully sourced ingredients, while also drawing upon global cuisines, reimagining flavors from the chef\'s travels with impressive imagination. Whimsy is a common thread throughout, as in a puffed pillow of crisp potato filled with "Fancy French Onion Dip" and caviar, or "The Dish That Made Us," a single woodfire-roasted octopus tentacle with black peppercorn sauce and pickled cherries.',
         website: 'https://localissacramento.com/',
         number: '+1 916-737-7699',
+        latitude: '38.56679214736793',
+        longitude: '-121.48303911786269',
       },
       {
         name: 'Kenzo',
@@ -42,6 +46,8 @@ async function main() {
           "Kenzo Tsujimoto's Napa Valley temple of traditional Japanese cuisine is a place to wash away worldly cares. Designed by his wife Natsuko, this 25-seat space is serene and minimal. The best seats are at the long counter, where diners can chat with the chefs and watch their meal being prepared. The kaiseki experience is beautifully composed, elegantly paced and may feature such exquisite presentations as the seasonal hassun—unveiling poached eggplant in dashi and seared Sonoma duck breast. Other standouts include straw-smoked Hokkaido scallop sashimi with jidori egg yolk sauce; and Wagyu beef tenderloin with a reduction of the estate's own Bordeaux-style blend. The sake variety is extraordinary, but Kenzo Estate’s own Napa-grown wines are also available by the flight.",
         website: 'https://www.kenzonapa.com/',
         number: '+1 707-294-2049',
+        latitude: '38.32821922368657',
+        longitude: '-122.3045016852633',
       },
     ];
 
